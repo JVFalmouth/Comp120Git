0013c59451bdc5dd315c088a049676b38943e5d3
JamesV <JV228882@falmouth.ac.uk>
2019-10-08 11:09:26 +0100

    I have edited the Tinkering Graphics program to work with some of the tasks set in the workshop pres.

--- Tinkering Graphics/tinkering graphics.py
+++ Tinkering Graphics/tinkering graphics.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
 
 def reduce_red(pic):
@@ -6,17 +6,35 @@ def reduce_red(pic):
     for x in range (pic.get_size()[0]):
         for y in range(pic.get_size()[1]):
             colour = list(pic.get_at((x,y)))
-            print(colour)
             recolour = colour[0]
             colour[0] = colour[2]
             colour[2] = recolour
             colour[2] = colour[2] * 0.5
             pic.set_at((x,y), colour)
 
+class Pic:
+    def __init__(self):
+        self.pic = pygame.image.load(picture_addr)
+        self.pos = (1, 1)
+
+class Block:
+
+    def __init__(self):
+        self.speed = 2
+        self.box_x = 300
+        self.box_y = 200
+        self.box_dir = 3
+        self.box_colour = (100, 0, 255)
+        self.size = 30
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.box_colour, (self.box_x, self.box_y, self.size, self.size))
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Green block moving all over the place.")
+    pygame.display.set_caption("Moving Block.")
 
     WHITE = (250, 250, 250)
     GREEN = (0, 250, 0)
@@ -25,26 +43,31 @@ def main():
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill(WHITE)
+    clock = pygame.time.Clock()
 
-    red_ball = pygame.image.load("red_ball.jpg")
-    red_ball_pos = (1, 1)
-    reduce_red(red_ball)
+    box = Block()
 
     while True:
+        clock.tick(60)
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
                 return
-        if pygame.key.get_pressed()[pygame.K_d] and red_ball_pos[0] < screen.get_size()[0]:
-            red_ball_pos = ((red_ball_pos[0] + 1), red_ball_pos[1])
-        if pygame.key.get_pressed()[pygame.K_a] and red_ball_pos[0] > 0:
-            red_ball_pos = ((red_ball_pos[0] - 1), red_ball_pos[1])
-        if pygame.key.get_pressed()[pygame.K_w] and red_ball_pos[1] > 0:
-            red_ball_pos = (red_ball_pos[0], (red_ball_pos[1] - 1))
-        if pygame.key.get_pressed()[pygame.K_s] and red_ball_pos[1] < screen.get_size()[1]:
-            red_ball_pos = (red_ball_pos[0], (red_ball_pos[1] + 1))
+        screen.fill(BLACK)
+        box.draw(screen)
+        if pygame.key.get_pressed()[pygame.K_d] and (box.box_x + box.size) < screen.get_size()[0]:
+            box.box_x += box.speed
+        if pygame.key.get_pressed()[pygame.K_a] and box.box_x > 0:
+            box.box_x -= box.speed
+        if pygame.key.get_pressed()[pygame.K_w] and box.box_y > 0:
+            box.box_y -= box.speed
+        if pygame.key.get_pressed()[pygame.K_s] and (box.box_y + box.size) < screen.get_size()[1]:
+            box.box_y += box.speed
 
+        """
+        red_ball_pos = (1, 1)
+        reduce_red(red_ball)
         screen.blit(background, (0, 0))
-        screen.blit(red_ball, (red_ball_pos))
+        screen.blit(red_ball, (red_ball_pos))"""
         pygame.display.flip()
 
 main()
